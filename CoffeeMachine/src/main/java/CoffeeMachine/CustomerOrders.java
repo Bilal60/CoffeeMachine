53524e49da871f876e5b5598653ac4416490b1d7
bilal60 <bilal.akar.upjv@gmail.com>
2020-01-26 13:50:22 +0100

    Le programme renvoie la boisson choisie (café, thé ou chocolat) avec 0 sucre et par conséquent 0 touillette

--- CoffeeMachine/src/main/java/CoffeeMachine/CustomerOrders.java
+++ CoffeeMachine/src/main/java/CoffeeMachine/CustomerOrders.java
@@ -2,4 +2,36 @@ package CoffeeMachine;
 
 public class CustomerOrders {
 	
+	private String command;
+
+	//Constructeur
+	public CustomerOrders(String command){
+		this.command = command;
+	}
+
+
+	public String drinkMakerMessage() {
+		//separartion de la chaine de caractères command suivant un délimiteur
+		String splitedCommand[] = this.command.split(":", -1);
+		String drinks ="";
+		String suggarAndStick = "";
+
+		for (int i = 0; i < splitedCommand.length; i++) {
+            if(splitedCommand[0].equals("T")){
+            	drinks = "tea";
+            }else if(splitedCommand[0].equals("H")){
+            	drinks = "chocolate";
+            }else if(splitedCommand[0].equals("C")){
+            	drinks = "coffee";
+            }
+
+            if(splitedCommand[1].equals("")){
+            	suggarAndStick = "no sugar - and therefore no stick";
+            }
+
+        }
+        String message = "Drink maker makes 1 "+drinks+" with "+suggarAndStick;
+        return message;
+	}
+	
 }
--- CoffeeMachine/src/test/java/CoffeeMachine/CustomerOrdersTest.java
+++ CoffeeMachine/src/test/java/CoffeeMachine/CustomerOrdersTest.java
@@ -7,8 +7,27 @@ import org.junit.jupiter.api.Test;
 class CustomerOrdersTest {
 
 	@Test
-	void test() {
-		System.out.println("ok");
+	void testTeaNoSugar() {
+		CustomerOrders order1 = new CustomerOrders("T::");
+		String actualMessage = order1.drinkMakerMessage();
+		String expectedMessage = "Drink maker makes 1 tea with no sugar - and therefore no stick";
+		assertEquals(expectedMessage, actualMessage, "Doit retourner un message qui contient 1 thé (tea), 0 sucre (sugar) et 0 touillette (stick)");
+	}
+	
+	@Test
+	void testCoffeeNoSugar() {
+		CustomerOrders order1 = new CustomerOrders("C::");
+		String actualMessage = order1.drinkMakerMessage();
+		String expectedMessage = "Drink maker makes 1 coffee with no sugar - and therefore no stick";
+		assertEquals(expectedMessage, actualMessage, "Doit retourner un message qui contient 1 café (coffee), 0 sucre (sugar) et 0 touillette (stick)");
+	}
+	
+	@Test
+	void testChocolateNoSugar() {
+		CustomerOrders order1 = new CustomerOrders("H::");
+		String actualMessage = order1.drinkMakerMessage();
+		String expectedMessage = "Drink maker makes 1 chocolate with no sugar - and therefore no stick";
+		assertEquals(expectedMessage, actualMessage, "Doit retourner un message qui contient 1 chocolat (chocolate), 0 sucre (sugar) et 0 touillette (stick)");
 	}
 
 }
