126cac46a42016647436805f91c3f481cca62588
bilal60 <bilal.akar.upjv@gmail.com>
2020-01-26 10:41:07 +0100

    Initialisation du projet

---
+++ CoffeeMachine/src/test/java/CoffeeMachine/CustomerOrdersTest.java
@@ -0,0 +1,14 @@
+package CoffeeMachine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerOrdersTest {
+
+	@Test
+	void test() {
+		System.out.println("ok");
+	}
+
+}
